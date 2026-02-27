78d0779ab405054419b1355406e68ffa4bdb0385
krishna-dtu <kj20130284870@gmail.com>
2026-02-27 19:26:30 +0530

    credentials move to .env

--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -2,17 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 /**
- * ✅ Firebase Config (Paste from Firebase Console)
+ * ✅ Firebase Config (from .env)
  * Project Settings → Web App → Config
  */
 const firebaseConfig = {
-  apiKey: "AIzaSyDwKs6DoCisxVo2TtMbvpIA5kDb3wBj0f4",
-  authDomain: "focusdesk-38386.firebaseapp.com",
-  projectId: "focusdesk-38386",
-  storageBucket: "focusdesk-38386.firebasestorage.app",
-  messagingSenderId: "959487543213",
-  appId: "1:959487543213:web:2196fe576f037081000324",
-  measurementId: "G-CQFD3BQSV6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 /**
  * ✅ Initialize Firebase App
